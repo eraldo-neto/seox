4cbd6d3a57aeeba3ee67e641bc6fe3dd542a0355
Eraldo dos Santos Neto <eraldo.santoneto@gmail.com>
2023-02-23 15:56:52 -0300

    Correction for SEO and Acessibiliti

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php
+
+include get_theme_file_path( 'inc/setup.php' );
+include get_theme_file_path( 'inc/utils/utils.php' );
--- template-part/common/advertisement.php
+++ template-part/common/advertisement.php
@@ -1,6 +1,12 @@
+<?php
+$categories = get_categories();
+
+$categoryLink = get_category_link( $categories[0]->term_id );
+?>
+
 <div class="advertisement">
     <p>publicidade</p>
-    <a href="">
+    <a href="<?= $categoryLink ?>" aria-label="publicidade">
         <div class="image"></div>
     </a>
 </div>
